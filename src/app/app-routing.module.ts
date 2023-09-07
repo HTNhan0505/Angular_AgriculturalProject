ad05aa30ac13b421f5c25b3ed11f246b86acf371
nhan055 <htnhan0505@gmail.com>
2023-09-07 15:17:31 +0700

    Done UI

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContainerComponent } from './Component/container/container.component';
+import { AdminComponent } from './Pages/admin/admin.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "", component: ContainerComponent
+  },
+  {
+    path: "admin", component: AdminComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContainerComponent } from './Component/container/container.component';
+import { HeaderComponent } from './Component/header/header.component';
+import { FooterComponent } from './Component/footer/footer.component';
+import { AdminComponent } from './Pages/admin/admin.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContainerComponent,
+    HeaderComponent,
+    FooterComponent,
+    AdminComponent
   ],
   imports: [
     BrowserModule,
