44f6bf8959717f3f460e4aea6a0e5974de339b98
nhan055 <htnhan0505@gmail.com>
2023-09-08 14:27:23 +0700

    Chua Responsive

--- src/app/Component/container/container.component.ts
+++ src/app/Component/container/container.component.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/model/product';
+import { DataService } from 'src/app/shared/data.service';
 
 @Component({
   selector: 'app-container',
   templateUrl: './container.component.html',
   styleUrls: ['./container.component.css']
 })
-export class ContainerComponent {
-
+export class ContainerComponent implements OnInit {
+  productList: Product[] = []
+  constructor(private data: DataService) { }
+  ngOnInit(): void {
+    this.getAllProducts()
+  }
+  getAllProducts() {
+    this.data.getAllProducts().subscribe(res => {
+      this.productList = res.map((e: any) => {
+        const data = e.payload.doc.data()
+        data.id = e.payload.doc.id
+        return data;
+      })
+    }, err => {
+      alert('Error fetching data')
+    })
+  }
 }
